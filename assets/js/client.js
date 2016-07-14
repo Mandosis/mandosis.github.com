9161f523d4e93047cbce5cb47b7b421c1a8ae74f
Chris Rabuse <rab233@live.com>
2016-07-13 20:35:50 -0500

    Add updated design, job title and remove trianglify

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,18 +1,1 @@
-var pattern = Trianglify({
-  width: window.innerWidth,
-  height: window.innerHeight,
-  x_colors: 'YlGnBu'
-});
 
-// document.body.appendChild(pattern.canvas())
-// pattern.png(document.getElementById('container'));
-var main = document.getElementsByClassName('pattern');
-console.log(main);
-var css = document.createAttribute('style');
-
-// Set backgound
-css.value = 'background: url(' + pattern.png() + '); background-repeat: no-repeat; background-size: auto; background-attachment: fixed;';
-
-main[0].setAttributeNode(css);
-
-// document.body.append(pattern.png('pattern.png'));
