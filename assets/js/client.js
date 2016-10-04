ea972fb816c19db4d36152ae8c6a66b24038b966
Chris Rabuse <rab233@live.com>
2016-10-04 00:51:42 -0500

    Add jQuery and portfolio section

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,1 +1,18 @@
+$(function() {
+  console.log('jQuery Loaded');
+  $(".btn-down").click(function() {
+    $(".portfolio").removeClass('hidden');
+    $("html, body").animate({
+      scrollTop: $(".portfolio").offset().top
+    }, 500, "swing", function() {
+      $(".intro").addClass('hidden');
 
+      $(".portfolio-title").css({"margin-top": "3em", "opacity": ""}).animate({
+        "margin-top": "2em",
+        opacity: '1'
+      }, 400);
+
+    });
+
+  })
+})
