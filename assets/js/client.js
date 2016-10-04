78ffffdbdffa1f978248f3bbe05bfea9c115aae3
Chris Rabuse <rab233@live.com>
2016-10-04 16:34:46 -0500

    Add scroll event to control navigation

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,3 +1,7 @@
+var currentIndex = 1;
+var lastIndex = 2;
+var canScroll = true;
+
 $(function() {
   console.log('jQuery Loaded');
   $(".btn-down").click(function() {
@@ -14,8 +18,74 @@ $(function() {
       }, 400, function() {
         $("body").css({overflow: "visible"});
       });
-
+      currentIndex++;
     });
 
-  })
-})
+  });
+
+  $("html, body").on('mousewheel', function(e) {
+    console.log(currentIndex);
+
+    var newIndex = null;
+    var index = currentIndex;
+    var up = (e.originalEvent.deltaY >= 0);
+
+    if (canScroll) {
+      if (up) {
+        if (currentIndex < 2) {
+          newIndex = currentIndex + 1;
+          currentIndex++;
+        }
+      } else {
+        if (currentIndex > 1) {
+          newIndex = currentIndex - 1;
+          currentIndex--;
+        }
+      }
+    }
+
+    if (newIndex) {
+      var currentElement = $("section[data-index="+ index +"]");
+      var nextElement = $("section[data-index=" + newIndex +"]");
+
+      $("body").css({overflow: 'hidden'});
+
+      nextElement.removeClass('hidden');
+      if (!up) {
+        $("html, body").animate({
+          scrollTop: currentElement.offset().top,
+        }, 0);
+      }
+
+      $("html, body").animate({
+        scrollTop: nextElement.offset().top,
+      }, {
+        duration: 500,
+        easing: 'swing',
+        start: function() {
+          canScroll = false;
+        },
+        complete: function() {
+          canScroll = true;
+          console.log('end function called')
+          currentElement.addClass('hidden');
+
+          var nextElementWithAnimation = $("section[data-index=" + newIndex +"] .animate")
+          $("body").css({overflow: "visible"});
+
+
+          nextElementWithAnimation.css({"margin-top": "3em", "opacity": ""}).animate({
+            "margin-top": "2em",
+            opacity: '1'
+          }, 400);
+
+          canScroll = true;
+
+        }
+      })
+    }
+
+  });
+
+
+});
