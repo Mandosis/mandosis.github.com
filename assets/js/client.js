01273357f6788eed763fa13e57cd735442f57f54
Chris Rabuse <rab233@live.com>
2016-10-04 01:50:13 -0500

    Hide scrollbar during portfolio entry animation

--- assets/js/client.js
+++ assets/js/client.js
@@ -2,15 +2,18 @@ $(function() {
   console.log('jQuery Loaded');
   $(".btn-down").click(function() {
     $(".portfolio").removeClass('hidden');
+    $("body").css({overflow: 'hidden'});
     $("html, body").animate({
-      scrollTop: $(".portfolio").offset().top
+      scrollTop: $(".portfolio").offset().top,
     }, 500, "swing", function() {
       $(".intro").addClass('hidden');
 
       $(".portfolio-title").css({"margin-top": "3em", "opacity": ""}).animate({
         "margin-top": "2em",
         opacity: '1'
-      }, 400);
+      }, 400, function() {
+        $("body").css({overflow: "visible"});
+      });
 
     });
 
