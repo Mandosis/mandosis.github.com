7614c627eb9e707c554117a7aed0f1f90eaeef26
Chris Rabuse <chris@reemohealth.com>
2020-02-13 22:17:29 -0600

    Basic 2020 design and content update

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "D:\\Chris\\Workspace\\mandosis.github.io";
+/******/ 	__webpack_require__.p = "C:\\Users\\Chris\\Workspace\\mandosis.github.io";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 1);
